67cdd59bfb88ed55258fe58250fc68664ba6032e
Isaque1984 <isac.silva1984.is@gmail.com>
2026-09-24 23:05:42 -0300

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,22 +1,43 @@
-const CACHE = "minha-meta-v1";
+const CACHE_NAME = "minha-meta-v2";
 
-self.addEventListener("install", function(event) {
-  self.skipWaiting();
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./meta-pro.html",
+  "./area-pro.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(["./", "./index.html", "./manifest.json"]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
